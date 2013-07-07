84bab30435947bde16178bf6166a088e9a6d387b
Matthew Yates <matt.yates.my@gmail.com>
2013-07-07 18:58:51 +0100

    Added jpeg encoding support

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"goFractal/fractal"
 	"image/png"
+	"image/jpeg"
 	"launchpad.net/gnuflag"
 	"os"
 	"runtime"
@@ -23,15 +24,18 @@ var cpus *int = gnuflag.Int(
 var threads *int = gnuflag.Int(
 	"threads", runtime.NumCPU(), "Number of rendering threads.",
 )
+var img_quality *int = gnuflag.Int(
+	"image-quality", 100, "Image quality (where applicable).",
+)
 
 func main() {
-	gnuflag.Parse(true)
-	xml_filename := gnuflag.Arg(0)
-
 	gnuflag.Usage = func() {
 		fmt.Print(usage)
 		gnuflag.PrintDefaults()
 	}
+
+	gnuflag.Parse(true)
+	xml_filename := gnuflag.Arg(0)
 
 	if xml_filename == "" || xml_filename[len(xml_filename)-3:] != "xml" {
 		gnuflag.Usage()
@@ -43,11 +47,13 @@ func main() {
 		*img_filename = xml_filename[:len(xml_filename)-3] + "png"
 	}
 
+	img_format := (*img_filename)[len(*img_filename)-3:]
+
 	runtime.GOMAXPROCS(*cpus)
 
-	renderer, err := fractal.ParseXml(xml_filename)
-	if err != nil {
-		fmt.Println(err)
+	renderer, render_err := fractal.ParseXml(xml_filename)
+	if render_err != nil {
+		fmt.Println(render_err)
 		return
 	}
 
@@ -65,6 +71,29 @@ func main() {
 
 	fmt.Print("\rFinished rendering in ", time.Since(t), " on ", *cpus, " CPUs with ", *threads, " threads\n")
 
-	imgFile, _ := os.Create(*img_filename)
-	png.Encode(imgFile, img)
+	imgFile, file_err := os.Create(*img_filename)
+
+	if file_err != nil {
+		fmt.Println(file_err)
+		return
+	}
+
+	var encoding_err error
+	switch(img_format){
+	case "png":
+		encoding_err = png.Encode(imgFile, img)
+	case "jpg":
+		opts := jpeg.Options{
+			Quality: *img_quality,
+		}
+		encoding_err = jpeg.Encode(imgFile, img, &opts)
+	default:
+		fmt.Println("Image format not supported: ", img_format)
+		return
+	}
+
+	if encoding_err != nil{
+		fmt.Println(encoding_err)
+		return
+	}
 }
