7e98d7fc85bf9210f5b3aa684b06b57ec82aa546
Matthew Yates <matt.yates.my@gmail.com>
2013-07-14 18:30:12 +0100

    improved filetype detection

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"goFractal/fractal"
-	"image/png"
 	"image/jpeg"
+	"image/png"
 	"launchpad.net/gnuflag"
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 	"time"
 )
 
 var usage string = "Usage: goFractal [OPTIONS]... [XML_RENDER_FILE]...\n" +
 	"Render the fractal defined in XML_RENDER_FILE\n"
 
-var img_filename *string = gnuflag.String(
+var imgFileName *string = gnuflag.String(
 	"output", "", "Output image filename.",
 )
 var cpus *int = gnuflag.Int(
@@ -24,7 +26,7 @@ var cpus *int = gnuflag.Int(
 var threads *int = gnuflag.Int(
 	"threads", runtime.NumCPU(), "Number of rendering threads.",
 )
-var img_quality *int = gnuflag.Int(
+var imgQuality *int = gnuflag.Int(
 	"image-quality", 100, "Image quality (where applicable).",
 )
 
@@ -35,25 +37,32 @@ func main() {
 	}
 
 	gnuflag.Parse(true)
-	xml_filename := gnuflag.Arg(0)
+	xmlFileName := gnuflag.Arg(0)
 
-	if xml_filename == "" || xml_filename[len(xml_filename)-3:] != "xml" {
+	if xmlFileName == "" || !strings.HasSuffix(xmlFileName, ".xml") {
+		fmt.Println("Sorry, only .xml input files are valid")
 		gnuflag.Usage()
 		return
 	}
 
-	if *img_filename == "" {
-		//strip off the .xml and replace with .png
-		*img_filename = xml_filename[:len(xml_filename)-3] + "png"
+	if *imgFileName == "" {
+		// strip off the .xml and replace with .png and use that as an output 
+		// if an output file is not specified
+		*imgFileName = xmlFileName[:strings.LastIndex(*imgFileName, ".")] + "png"
 	}
 
-	img_format := (*img_filename)[len(*img_filename)-3:]
+	imgFormat := (*imgFileName)[strings.LastIndex(*imgFileName, ".") + 1:]
 
 	runtime.GOMAXPROCS(*cpus)
 
-	renderer, render_err := fractal.ParseXml(xml_filename)
-	if render_err != nil {
-		fmt.Println(render_err)
+	renderer, renderErr := fractal.ParseXml(xmlFileName)
+	if renderErr != nil {
+		fmt.Println(renderErr)
+		return
+	}
+
+	if !(imgFormat == "png" || imgFormat == "jpg" || imgFormat == "jpeg"){
+		fmt.Println("Sorry, image format", imgFormat, "is not supported")
 		return
 	}
 
@@ -63,37 +72,35 @@ func main() {
 	// wait untill it is completed
 	t := time.Now()
 	for renderer.Rendering() {
-		prog_str := strconv.FormatFloat(100*renderer.Progress(), 'f', 1, 64)
-		fmt.Print("\r", prog_str, "%")
+		progStr := strconv.FormatFloat(100*renderer.Progress(), 'f', 1, 64)
+		fmt.Print("\r", progStr, "%")
 	}
 
 	img := renderer.GetImage()
 
 	fmt.Print("\rFinished rendering in ", time.Since(t), " on ", *cpus, " CPUs with ", *threads, " threads\n")
 
-	imgFile, file_err := os.Create(*img_filename)
-
-	if file_err != nil {
-		fmt.Println(file_err)
+	imgFile, fileErr := os.Create(*imgFileName)
+	if fileErr != nil {
+		fmt.Println(fileErr)
 		return
 	}
 
-	var encoding_err error
-	switch(img_format){
+	var encodingErr error
+	switch imgFormat {
 	case "png":
-		encoding_err = png.Encode(imgFile, img)
-	case "jpg":
+		encodingErr = png.Encode(imgFile, img)
+	case "jpg", "jpeg":
 		opts := jpeg.Options{
-			Quality: *img_quality,
+			Quality: *imgQuality,
 		}
-		encoding_err = jpeg.Encode(imgFile, img, &opts)
+		encodingErr = jpeg.Encode(imgFile, img, &opts)
 	default:
-		fmt.Println("Image format not supported: ", img_format)
-		return
+		encodingErr = errors.New("Image format not supported: " + imgFormat)
 	}
 
-	if encoding_err != nil{
-		fmt.Println(encoding_err)
+	if encodingErr != nil {
+		fmt.Println(encodingErr)
 		return
 	}
 }
